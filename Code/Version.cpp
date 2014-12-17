ddff264225662de44008c46e5c3ffd075e13e5a4
David Simmerson <dsimmerson@gmail.com>
2014-12-17 12:26:55 -0500

    Added the Version.cpp file to the hub of git

---
+++ Code/Version.cpp
@@ -0,0 +1,6 @@
+#include "Engine.h"
+const unsigned long mudVersion = 977+1;
+const char *mudBuildType = "ALPHA";
+const char *mudEngineName = "InfectEngine";
+const char *mudBackupName = "backup-20141217-977.tgz";
+const char *mudCompiler = "g++";
